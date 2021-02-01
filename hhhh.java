d8ab5bbc19037937c6da813beba9143787be1b75
Dove-swift <78304046+Dove-swift@users.noreply.github.com>
2021-02-01 23:07:20 +0800

    Update hhhh.java

--- hhhh.java
+++ hhhh.java
@@ -8,11 +8,11 @@ public class hhhh {
 
         public static void main(String[] args) {
             System.out.println("请输入数字，以英文逗号分隔,:");
-            Scanner scanner = new Scanner(System.in); //首先得到一行输入(以逗号分隔的一组数据),字符串形式str
-            String str = scanner.nextLine(); //去掉str的分割逗号，得到字符数组strRating
-            String[] strRating = str.split(","); //整数数组
+            Scanner scanner = new Scanner(System.in); 
+            String str = scanner.nextLine(); 
+            String[] strRating = str.split(","); 
             int[] nums = new int[strRating.length];
-            for (int i = 0; i < strRating.length; i++) { //将字符数组的元素转换成整数数组
+            for (int i = 0; i < strRating.length; i++) {
                 nums[i] = Integer.parseInt(strRating[i]);
             }
             bubble_sort(nums);
@@ -20,11 +20,11 @@ public class hhhh {
                 System.out.println(nums[i]);
             }
             System.out.println("请再输入数字，以英文逗号分隔,:");
-            Scanner sc = new Scanner(System.in); //首先得到一行输入(以逗号分隔的一组数据),字符串形式str
-            String str2 = sc.nextLine(); //去掉str的分割逗号，得到字符数组strRating
-            String[] str2Rating = str2.split(","); //整数数组
+            Scanner sc = new Scanner(System.in); 
+            String str2 = sc.nextLine(); 
+            String[] str2Rating = str2.split(","); 
             int[] nums2 = new int[str2Rating.length];
-            for (int i = 0; i < str2Rating.length; i++) { //将字符数组的元素转换成整数数组
+            for (int i = 0; i < str2Rating.length; i++) { 
                 nums2[i] = Integer.parseInt(str2Rating[i]);
             }
             bubble_sort(nums2);
